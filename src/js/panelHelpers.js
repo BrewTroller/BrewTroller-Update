8ec7e648d5df19a7d33f6814a9b1483bca905288
Eric Yanush <ericyanush@gmail.com>
2015-07-28 00:10:57 -0600

    Implement dependant panel set creation helper

--- src/js/panelHelpers.js
+++ src/js/panelHelpers.js
@@ -40,18 +40,105 @@ var createRadioPanel = function(opt) {
         return s;
     }
 
-    var createDepenendantPanelSet = function(optSet) {
+    var createDependantPanelSet = function(opt) {
 
-        //TODO: Finsh implementing panel build / master listening logic
-        throw "INCOMPLETE IMPELEMENTATION";
+        var set = {"option": opt,
+                   "panel": null,
+                   "wrapper": null,
+                   "dependants": [],
+                   "setup": null,
+                   "masterChangeHandler": null
+        };
 
         //Create the dependant option panel
-        var p = Polymer.Base.create('dependant-options-panel', {
+        set.panel = Polymer.Base.create('dependant-options-panel', {
             "id": opt.id + "MasterPanel",
             "optionDescription": opt.description,
             "optionTitle": opt.title || opt.id,
-            
+            "masterOption": opt.master,
+            "boolOptions": opt.options.bool,
+            "rangeOptions": opt.options.slider,
+            "enumOptions": []
         });
+        set.wrapper = Polymer.Base.create('neon-animatable', {});
+        Polymer.dom(set.wrapper).appendChild(set.panel);
 
-        //TODO: Create bound functions to handle the firing of the master change event, and the initial insertion of the master
-    }
+        //Create the slave panels
+        for (dep in opt.dependants) {
+            var dpanel = null;
+            switch(opt.dependants[dep].type) {
+                case "switch":
+                    dpanel = createSwitchPanel(opt.dependants[dep]);
+                    break;
+                case "slider":
+                    dpanel = createSliderPanel(opt.dependants[dep]);
+                    break;
+                case "radio":
+                    dpanel = createRadioPanel(opt.dependants[dep]);
+                    break;
+            }
+            set.dependants.push(dpanel);
+        }
+
+        var masterChange = function(e) {
+            //ensure all the dependant panels are in the container
+            if (e.detail.newVal) {
+                var masterWrapper = set.wrapper;
+                var container = Polymer.dom(masterWrapper).parentNode;
+                var insertFunc = null;
+                if (masterWrapper.nextSibling) {
+                    insertFunc = function(newNode) {
+                        Polymer.dom(container).insertBefore(newNode, masterWrapper.nextSibling);
+                    }
+                }
+                else {
+                    insertFunc = Polymer.dom(container).appendChild;
+                }
+
+                for (dep in set.dependants) {
+                    if (!container.contains(set.dependants[dep].wrapper)) {
+                        insertFunc(set.dependants[dep].wrapper);
+                    }
+                }
+            } 
+            //ensure all the dependant panels have been removed
+            else {
+                var masterWrapper = set.wrapper;
+                var container = Polymer.dom(masterWrapper).parentNode;
+
+                for (dep in set.dependants) {
+                    if (container.contains(set.dependants[dep].wrapper)) {
+                        Polymer.dom(container).removeChild(set.dependants[dep].wrapper);
+                    }
+                }
+            }
+        }
+
+        var setup = function() {
+            //If the master is enabled, insert all the dependants after it
+            if (set.panel.masterOption.on) {
+                var masterWrapper = set.wrapper;
+                var container = masterWrapper.parentElement;
+
+                var insertFunc = null;
+                if (masterWrapper.nextSibling) {
+                    insertFunc = function(newNode) {
+                        Polymer.dom(container).insertBefore(newNode, masterWrapper.nextSibling);
+                    }
+                }
+                else {
+                    insertFunc = Polymer.dom(container).appendChild;
+                }
+
+                //insert each dependant panel
+                for (p in set.dependants) {
+                    insertFunc(set.dependants[p].wrapper);
+                }
+            }
+            set.masterChangeHandler = masterChange.bind(this);
+            set.panel.addEventListener("masterOptionChange", set.masterChangeHandler);
+        }
+        set.setup = setup.bind(this);
+
+        return set;
+    }   
